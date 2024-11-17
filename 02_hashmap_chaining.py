13ba7796cd293e71ce598669bef301f105ff6624
Vítězslav Kříž <49568741+sprtokiller@users.noreply.github.com>
2024-11-17 19:17:37 +0100

    Create 02_hashmap_chaining.py

---
+++ 02_hashmap_chaining.py
@@ -0,0 +1,43 @@
+import time
+import random
+
+class ChainingHashMap:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def hash_function(self, key):
+        # TODO: Implementovat hashovací funkci
+        pass
+
+    def add(self, key, value):
+        # TODO: Přidat prvek s klíčem "key" a hodnotou "value" do hashmapy
+        pass
+
+    def find(self, key):
+        # TODO: Najít prvek s klíčem "key" v hashmapě a vrátit jeho hodnotu
+        pass
+
+def measure_time(operation, *args):
+    start = time.time()
+    result = operation(*args)
+    end = time.time()
+    return result, end - start
+
+if __name__ == "__main__":
+    hash_map = ChainingHashMap(1000)
+    data = list(range(1, 100001))
+    random.shuffle(data)
+
+    # Přidávání po dávkách
+    for i in range(0, len(data), 10000):
+        batch = data[i:i+10000]
+        _, duration = measure_time(lambda b: [hash_map.add(key, f"value{key}") for key in b], batch)
+        print(f"Adding batch {i//10000 + 1}: {duration:.6f} s")
+
+    # Hledání po dávkách
+    random.shuffle(data)
+    for i in range(0, len(data), 10000):
+        batch = data[i:i+10000]
+        _, duration = measure_time(lambda b: [hash_map.find(key) for key in b], batch)
+        print(f"Finding batch {i//10000 + 1}: {duration:.6f} s")
