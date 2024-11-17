6f919b72dfe0f5f09fa430bdc7592a74cf235e89
Vítězslav Kříž <49568741+sprtokiller@users.noreply.github.com>
2024-11-17 19:17:02 +0100

    Create 01_pole.py

---
+++ 01_pole.py
@@ -0,0 +1,34 @@
+import time
+import random
+
+def measure_time(operation, *args):
+    start = time.time()
+    result = operation(*args)
+    end = time.time()
+    return result, end - start
+
+def add_elements(array, elements):
+    # TODO: Přidat všechny prvky z "elements" do pole "array"
+    pass
+
+def find_elements(array, elements):
+    # TODO: Najít všechny prvky z "elements" v poli "array"
+    pass
+
+if __name__ == "__main__":
+    array = []
+    data = list(range(1, 100001))
+    random.shuffle(data)
+
+    # Přidávání po dávkách
+    for i in range(0, len(data), 10000):
+        batch = data[i:i+10000]
+        _, duration = measure_time(add_elements, array, batch)
+        print(f"Adding batch {i//10000 + 1}: {duration:.6f} s")
+
+    # Hledání po dávkách
+    random.shuffle(data)
+    for i in range(0, len(data), 10000):
+        batch = data[i:i+10000]
+        _, duration = measure_time(find_elements, array, batch)
+        print(f"Finding batch {i//10000 + 1}: {duration:.6f} s")
