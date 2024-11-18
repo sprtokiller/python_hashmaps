c39545e607ba8b7a4eda2ce9c7bd69a9acb56542
JanBulir <147809266+JanBulir@users.noreply.github.com>
2024-11-18 12:08:40 +0100

    Add files via upload

--- 02_hashmap_chaining.py
+++ 02_hashmap_chaining.py
@@ -7,16 +7,22 @@ class ChainingHashMap:
         self.table = [[] for _ in range(size)]
 
     def hash_function(self, key):
-        # TODO: Implementovat hashovací funkci
-        pass
+        return hash(key) % self.size 
 
     def add(self, key, value):
-        # TODO: Přidat prvek s klíčem "key" a hodnotou "value" do hashmapy
-        pass
+        index = self.hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value  
+                return
+        self.table[index].append([key, value])
 
     def find(self, key):
-        # TODO: Najít prvek s klíčem "key" v hashmapě a vrátit jeho hodnotu
-        pass
+        index = self.hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        return None 
 
 def measure_time(operation, *args):
     start = time.time()
