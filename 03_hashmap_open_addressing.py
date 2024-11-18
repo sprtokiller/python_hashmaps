63de28e89239d3263021d5a9199fa8cc9bdd373d
3.D Masopust Benjamin <Benjamin.Masopust@student.sps-cl.cz>
2024-11-18 12:09:56 +0100

    18.11

--- 03_hashmap_open_addressing.py
+++ 03_hashmap_open_addressing.py
@@ -4,19 +4,54 @@ import random
 class OpenAddressingHashMap:
     def __init__(self, size):
         self.size = size
-        self.table = [None] * size
+        self.table = [None] * size  # Tabulka inicializovaná na None
+        self.deleted = object()  # Speciální marker pro smazané prvky
 
     def hash_function(self, key):
-        # TODO: Implementovat hashovací funkci
-        pass
+        # Jednoduchá hashovací funkce
+        return key % self.size
 
     def add(self, key, value):
-        # TODO: Přidat prvek s klíčem "key" a hodnotou "value" do hashmapy
-        pass
+        index = self.hash_function(key)
+        original_index = index
+        while self.table[index] is not None and self.table[index] is not self.deleted:
+            stored_key, _ = self.table[index]
+            if stored_key == key:  # Aktualizace hodnoty, pokud klíč existuje
+                self.table[index] = (key, value)
+                return
+            index = (index + 1) % self.size
+            if index == original_index:  # Pokud se vrátíme na původní index
+                raise Exception("HashMap is full")
+
+        # Přidání nového prvku
+        self.table[index] = (key, value)
 
     def find(self, key):
-        # TODO: Najít prvek s klíčem "key" v hashmapě a vrátit jeho hodnotu
-        pass
+        index = self.hash_function(key)
+        original_index = index
+        while self.table[index] is not None:
+            if self.table[index] is not self.deleted:
+                stored_key, stored_value = self.table[index]
+                if stored_key == key:
+                    return stored_value
+            index = (index + 1) % self.size
+            if index == original_index:  # Pokud se vrátíme na původní index
+                break
+        return None  # Klíč nebyl nalezen
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        original_index = index
+        while self.table[index] is not None:
+            if self.table[index] is not self.deleted:
+                stored_key, _ = self.table[index]
+                if stored_key == key:
+                    self.table[index] = self.deleted  # Označit jako smazané
+                    return True
+            index = (index + 1) % self.size
+            if index == original_index:
+                break
+        return False  # Klíč nebyl nalezen pro smazání
 
 def measure_time(operation, *args):
     start = time.time()
@@ -25,19 +60,19 @@ def measure_time(operation, *args):
     return result, end - start
 
 if __name__ == "__main__":
-    hash_map = OpenAddressingHashMap(200000)
-    data = list(range(1, 100001))
+    hash_map = OpenAddressingHashMap(200000)  # Velikost tabulky
+    data = list(range(1, 100001))  # Data od 1 do 100000
     random.shuffle(data)
 
     # Přidávání po dávkách
     for i in range(0, len(data), 10000):
         batch = data[i:i+10000]
         _, duration = measure_time(lambda b: [hash_map.add(key, f"value{key}") for key in b], batch)
-        print(f"Adding batch {i//10000 + 1}: {duration:.6f} s")
+        print(f"Přidání dávky {i//10000 + 1}: {duration:.6f} s")
 
     # Hledání po dávkách
     random.shuffle(data)
     for i in range(0, len(data), 10000):
         batch = data[i:i+10000]
         _, duration = measure_time(lambda b: [hash_map.find(key) for key in b], batch)
-        print(f"Finding batch {i//10000 + 1}: {duration:.6f} s")
+        print(f"Hledání dávky {i//10000 + 1}: {duration:.6f} s")
