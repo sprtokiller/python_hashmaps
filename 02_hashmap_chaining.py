c13326303902c61037054e177045e43c2745bc47
3.E Tomíšek Ondřej <Ondrej.Tomisek@student.sps-cl.cz>
2024-11-18 12:17:52 +0100

    Ulozeni

--- 02_hashmap_chaining.py
+++ 02_hashmap_chaining.py
@@ -8,15 +8,24 @@ class ChainingHashMap:
 
     def hash_function(self, key):
         # TODO: Implementovat hashovací funkci
-        pass
+        return hash(key) % self.size
 
     def add(self, key, value):
         # TODO: Přidat prvek s klíčem "key" a hodnotou "value" do hashmapy
-        pass
+        index = self.hash_function(key)
+        for i, (k, v) in enumerate(self.table[index]):
+            if k == key:
+                self.table[index][i] = (key, value)
+                return
+        self.table[index].append((key, value))
 
     def find(self, key):
         # TODO: Najít prvek s klíčem "key" v hashmapě a vrátit jeho hodnotu
-        pass
+        index = self.hash_function(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+            return None
 
 def measure_time(operation, *args):
     start = time.time()
